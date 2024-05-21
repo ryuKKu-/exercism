87926632cfefd1ad7b8010c3b06c3a922792b52c
ryuK <ryukku.raymond@gmail.com>
2024-05-21 10:19:24 +0200

    scrabble

---
+++ fsharp/scrabble-score/ScrabbleScore.fs
@@ -0,0 +1,16 @@
+module ScrabbleScore
+
+let score (word: string) =
+    let score' c =
+        match c with
+        | 'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' -> 1
+        | 'd' | 'g' -> 2
+        | 'b' | 'c' | 'm' | 'p' -> 3
+        | 'f' | 'h' | 'v' | 'w' | 'y' -> 4
+        | 'k' -> 5
+        | 'j' | 'x' -> 8
+        | 'q' | 'z' -> 10
+        | _ -> 0
+    
+    word.ToLowerInvariant()
+    |> Seq.sumBy score'
